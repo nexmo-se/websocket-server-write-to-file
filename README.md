26a7552ce5c3e7fb83f61d78c535b9a5cda841f6
Tony Chan <tony.chan@vonage.com>
2026-02-20 14:15:34 -0800

    initial

---
+++ README.md
@@ -0,0 +1,46 @@
+# WebSocket server - Write received text streaming to file
+
+## About this Connector code
+
+This connector makes use of the WebSockets feature of [Vonage Voice API](https://developer.vonage.com/en/voice/voice-api/concepts/websockets).
+
+## Set up
+
+### Folder
+
+Make sure there is a folder named "recordings" where the log files are created.
+
+### Local deployment
+
+For a `local deployment`, you may use ngrok (an Internet tunneling service) for this WebSocket server application with [an ngrok tunnel](https://ngrok.com/docs/agent/config/v2/#tunnel-configurations).
+
+To do that, [download and install ngrok](https://ngrok.com/download).</br>
+Sign in or sign up with [ngrok](https://ngrok.com/), from the ngrok web UI menu, follow the **Setup and Installation** guide.
+
+Set up a domain to forward to the local port 6000 (as this Connector application will be listening on port 6000).
+
+Start ngrok to start the tunnel that forwards to local ports 6000, e.g.<br>
+`ngrok start httpbin` (per this [sample yaml configuration file](https://ngrok.com/docs/agent/config/v2/#define-two-tunnels-named-httpbin-and-demo), but needs port 6000 as actual values)
+
+
+Have Node.js installed on your system, this application has been tested with Node.js version 22.16.<br>
+
+Install node modules with the command:<br>
+ ```bash
+npm install
+```
+
+Launch the application:<br>
+```bash
+node websocket-server-write-to-file
+```
+
+Default local (not public!) of this application server `port` is: 8000.
+
+
+
+
+
+
+
+
---
+++ websocket-server-write-to-file.js
@@ -0,0 +1,156 @@
+'use strict'
+
+//-------------
+
+require('dotenv').config();
+
+//--
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+require('express-ws')(app);
+
+app.use(bodyParser.json());
+
+const webSocket = require('ws');
+const axios = require('axios');
+const fsp = require('fs').promises;
+const moment = require('moment');
+const path = require('path'); // ensure compatibility between Windwos, MacOS, and Linux, for file writing in a given folder
+
+//---- CORS policy - Update this section as needed ----
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "OPTIONS,GET,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+  next();
+});
+
+// //-- Backtrack audio payload handling --
+
+// const circularBuffer = require("circular-buffer");
+
+// // circular buffer size
+// const cbLength = Math.round(process.env.BACKTRACK_PAYLOAD_DURATION / 20);
+
+// //--- Record all audio ? --
+
+// let recordAllAudio = false;
+// if (process.env.RECORD_ALL_AUDIO == "true") { recordAllAudio = true };
+
+//-------------------------
+
+// ONLY if needed - For self-signed certificate in chain - In test environment
+// Must leave next line as a comment in production environment
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
+//--- Websocket server ---
+
+app.ws('/socket', async (ws, req) => {
+
+  console.log('>>> WebSocket established');
+
+  let fileName = null;
+
+  let msgCount = 0;
+  let firstMessage = true;
+
+  //---------------
+
+  ws.on('message', async (msg) => {
+    
+    if (typeof msg === "string") {
+    
+      // console.log("\n>>> Websocket text message - raw :", msg);
+
+      const sessionId = JSON.parse(msg).sessionId;
+
+      if (!fileName) {
+
+        console.log('sessionId:', sessionId);
+
+        fileName = path.join('recordings', moment(Date.now()).format('YYYY-MM-DD_HH-mm-ss-SSS') + '_' + sessionId + '.txt'); // using server local time
+        // fileName = path.join('recordings', moment.utc(Date.now()).format('YYYY-MM-DD_HH-mm-ss-SSS') + '_' + sessionId + '.txt'); // using UTC
+
+        try {
+          await fsp.writeFile(fileName, '');
+        } catch(e) {
+          console.log('>>> Error creating file', fileName, e);
+        }
+
+        try {
+          fsp.appendFile(fileName, "[");
+        } catch(error) {
+          console.log(">>> Error writing to file", fileName, error);
+        }
+
+      } else {
+
+        if (firstMessage) {
+
+          try {
+              fsp.appendFile(fileName, msg);
+            } catch(error) {
+              console.log(">>> Error writing to file", fileName, error);
+            }
+
+          firstMessage = false;   
+
+        } else {
+
+          try {
+            fsp.appendFile(fileName, "," + msg);
+          } catch(error) {
+            console.log(">>> Error writing to file", fileName, error);
+          }
+
+        }    
+
+        // msgCount++;
+        // console.log('Messages count:', msgCount);
+    
+
+      }
+    
+    } else {
+
+      console.log("\n>>> Websocket binary message:", msg);
+
+    }
+
+  });
+
+  //--
+
+  ws.on('close', async () => {
+
+    try {
+      fsp.appendFile(fileName, "]");
+    } catch(error) {
+      console.log(">>> Error writing to file", fileName, error);
+    }
+
+    console.log("WebSocket closed");
+    
+  });
+
+});  
+
+//--- If this application is hosted on VCR (Vonage Cloud Runtime) serverless infrastructure --------
+
+app.get('/_/health', async(req, res) => {
+
+  res.status(200).send('Ok');
+
+});
+
+//=========================================
+
+const port = process.env.VCR_PORT || process.env.PORT || 6000;
+
+app.listen(port, () => console.log(`Connector application listening on port ${port}!`));
+
+//------------
+
